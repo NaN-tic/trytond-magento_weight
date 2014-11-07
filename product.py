98429b871d8f9d5170e81c253c7a201dab51ae29
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:26:29 +0100

    Cleanup tests

--- product.py
+++ product.py
@@ -1,6 +1,6 @@
-#This file is part magento_weight module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part magento_weight module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import PoolMeta
 
 __all__ = ['Product']
