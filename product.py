6af64f81abe172e9898b9b97a5330e255d3db834
resteve <resteve@zikzakmedia.com>
2016-02-08 16:03:53 +0100

    Remove Base External Mapping

--- product.py
+++ product.py
@@ -1,27 +1,34 @@
 # This file is part magento_weight module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 
 __all__ = ['Product']
 __metaclass__ = PoolMeta
 
 
 class Product:
-    "Product Variant"
     __name__ = "product.product"
 
     @classmethod
-    def magento_template_dict2vals(self, shop, values):
-        '''
-        Add weight in Magento values to Template
-        :param shop: obj
-        :param values: dict from Magento Product API
-        return dict
-        '''
-        vals = super(Product, self).magento_template_dict2vals(shop, values)
+    def magento_import_product(cls, values, shop=None):
+        vals = super(Product, cls).magento_import_product(values, shop)
+
         weight = values.get('weight')
-        if weight and shop.esale_weight_uom:
+        if shop and weight and shop.esale_weight_uom:
             vals['weight'] = float(weight)
             vals['weight_uom'] = shop.esale_weight_uom
+        return vals
+
+    @classmethod
+    def magento_export_product(cls, app, product, shop=None, lang='en_US'):
+        Uom = Pool().get('product.uom')
+
+        vals = super(Product, cls).magento_export_product(app, product, shop, lang)
+
+        weight = product.weight
+        if shop and shop.esale_weight_uom:
+            if product.weight_uom.id != shop.esale_weight_uom.id:
+                weight = Uom.compute_qty(product.weight_uom, shop.esale_weight_uom)
+        vals['weight'] = weight
         return vals
